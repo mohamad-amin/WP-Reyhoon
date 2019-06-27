9c831abc204219a8d1cf024cf5b812685e992f69
Mohamad Amin <mohamadmain@cafebazaar.ir>
2019-06-27 15:10:14 +0430

    Added mongo-db to app.js

--- Project/Back-Reyhoon/app.js
+++ Project/Back-Reyhoon/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// MongoDB setup
+mongoose.connect("mongodb://localhost/reyhoon", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Database Error!"));
+db.once("open", function() {
+  console.log("Database is alive!");
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
